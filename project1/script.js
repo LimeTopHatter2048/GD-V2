e1737e43fe62959ebc977865f0ca45d8364b668e
Leonaitasi Mafileo <leonaitasim@gmail.com>
2025-05-05 11:41:47 +1200

    animate()-animationStates, spriteAnimations

--- project1/script.js
+++ project1/script.js
@@ -7,16 +7,73 @@ const playerImage = new Image();
 playerImage.src = 'img/shadow_dog.png';
 const spriteWidth = 575;//width-px/Com
 const spriteHeight = 523;//height-px/row
-let frameX=0;
-let frameY=4;
+let playerState = 'getHit';
+
 let gameFrame = 0;
 const staggerFrames = 5;
+const spriteAnimations = [];
+const animationStates = [
+    {
+        name:'idle',
+        frames: 7,
+    },
+    {
+        name:'jump',
+        frames: 7,
+    },
+    {
+        name:'fall',
+        frames: 7,
+    },
+    {
+        name:'run',
+        frames: 9,
+    },
+    {
+        name:'dizzy',
+        frames: 11,
+    },
+    {
+        name:'sit',
+        frames: 5,
+    },
+    {
+        name:'roll',
+        frames: 7,
+    },
+    {
+        name:'bite',
+        frames: 7,
+    },
+    {
+        name:'ko',
+        frames: 12,
+    },
+    {
+        name:'getHit',
+        frames: 4,
+    },
+];
+animationStates.forEach((state, index)=> {
+    let frames = {
+        loc: [],
+    }
+    for (let j = 0; j < state.frames; j++){
+        let positionX = j * spriteWidth;
+        let positionY = index * spriteHeight;
+        frames.loc.push({x: positionX, y: positionY});
+    }
+    spriteAnimations[state.name] = frames;
+});
+console.log(animationStates);
+console.log(spriteAnimations);
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    let position = Math.floor(gameFrame/staggerFrames)%4;
-    frameX = spriteWidth * position;
-    ctx.drawImage(playerImage, frameX, frameY*spriteHeight, spriteWidth, spriteHeight, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    let position = Math.floor(gameFrame/staggerFrames)%spriteAnimations[playerState].loc.length;
+    let frameX = spriteWidth * position;
+    let frameY = spriteAnimations[playerState].loc[position].y;
+    ctx.drawImage(playerImage, frameX, frameY, spriteWidth, spriteHeight, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
     gameFrame++;
     requestAnimationFrame(animate);
