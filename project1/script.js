517f28105af431f6d6a6966b6115a1d08b047c53
Leonaitasi Mafileo <leonaitasim@gmail.com>
2025-05-05 11:09:08 +1200

    animate() - position

--- project1/script.js
+++ project1/script.js
@@ -8,19 +8,16 @@ playerImage.src = 'img/shadow_dog.png';
 const spriteWidth = 575;//width-px/Com
 const spriteHeight = 523;//height-px/row
 let frameX=0;
-let frameY=1;
+let frameY=4;
 let gameFrame = 0;
 const staggerFrames = 5;
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    //ctx.fillRect(50,50,100,100);
-    //ctx.drawImage(image, sx, sy, sw, sh, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.drawImage(playerImage, frameX*spriteWidth, frameY*spriteHeight, spriteWidth, spriteHeight, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    if(gameFrame % staggerFrames == 0){
-        if (frameX < 6) frameX++;
-        else frameX = 0;
-    }
+    let position = Math.floor(gameFrame/staggerFrames)%4;
+    frameX = spriteWidth * position;
+    ctx.drawImage(playerImage, frameX, frameY*spriteHeight, spriteWidth, spriteHeight, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
     gameFrame++;
     requestAnimationFrame(animate);
 }
