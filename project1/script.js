4c7bf4191918363f4f8899acebed8406ba267340
Leonaitasi Mafileo <leonaitasim@gmail.com>
2025-05-05 10:47:49 +1200

    animate()-frame1

--- project1/script.js
+++ project1/script.js
@@ -5,11 +5,18 @@ const CANVAS_HEIGHT = canvas.height =500;
 
 const playerImage = new Image();
 playerImage.src = 'img/shadow_dog.png';
+const spriteWidth = 575;//width-px/Com
+const spriteHeight = 523;//height-px/row
+let frameX=0;
+let frameY=1;
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.fillRect(50,50,100,100);
-    ctx.drawImage(playerImage, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    //ctx.fillRect(50,50,100,100);
+    //ctx.drawImage(image, sx, sy, sw, sh, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctx.drawImage(playerImage, frameX*spriteWidth, frameY*spriteHeight, spriteWidth, spriteHeight, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    if (frameX < 6) frameX++;
+    else frameX = 0;
     requestAnimationFrame(animate);
 }
 animate();
