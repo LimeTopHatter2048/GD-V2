a263c80ecf02b163a2fdf172fab872aeb9fe2d28
Leonaitasi Mafileo <leonaitasim@gmail.com>
2025-05-05 11:00:49 +1200

    animate()-gameFrame%staggerFrames

--- project1/script.js
+++ project1/script.js
@@ -9,14 +9,19 @@ const spriteWidth = 575;//width-px/Com
 const spriteHeight = 523;//height-px/row
 let frameX=0;
 let frameY=1;
+let gameFrame = 0;
+const staggerFrames = 5;
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     //ctx.fillRect(50,50,100,100);
     //ctx.drawImage(image, sx, sy, sw, sh, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     ctx.drawImage(playerImage, frameX*spriteWidth, frameY*spriteHeight, spriteWidth, spriteHeight, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    if (frameX < 6) frameX++;
-    else frameX = 0;
+    if(gameFrame % staggerFrames == 0){
+        if (frameX < 6) frameX++;
+        else frameX = 0;
+    }
+    gameFrame++;
     requestAnimationFrame(animate);
 }
 animate();
