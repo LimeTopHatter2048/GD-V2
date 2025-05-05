f5dc17ff0482cba20f725126b75f074ea754dd5a
Leonaitasi Mafileo <leonaitasim@gmail.com>
2025-05-05 14:06:59 +1200

    Layer{}-speedModifier >x2 = width + x -speed;

--- parallax backgrounds/script.js
+++ parallax backgrounds/script.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-const CANVAS_WIDTH = canvas.width =500;
-const CANVAS_HEIGHT = canvas.height =300;
+const CANVAS_WIDTH = canvas.width =800/2;
+const CANVAS_HEIGHT = canvas.height =700/2;
 let gameSpeed = 15;
 
 const backgroundLayer1 = new Image();
@@ -19,20 +19,51 @@ backgroundLayer4.src = 'img/layer-4.png';
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = 'img/layer-5.png';
 
-const spriteWidth = 2400;//2400-px/1
-const spriteHeight = 720;//720-px/1
+const spriteWidth = 2400/2;//2400-px/2
+const spriteHeight = 720/2;//720-px/2
 
-let x = 0;
-let x2 = 2400;
+class Layer {
+    constructor(image, speedModifier){
+        this.x =0;
+        this.y =0;
+        this.width = 2400/2;
+        this.height = 700/2;
+        this.x2 = this.width;
+        this.image = image
+        this.speedModifier = speedModifier;
+        this.speed = gameSpeed * this.speedModifier;
+    }
+    update(){
+        this.speed = gameSpeed * this.speedModifier;
+        if (this.x <= -this.width){
+            this.x = this.width + this.x2 - this.speed;
+        }
+        if (this.x2 <= -this.width){
+            this.x2 = this.width + this.x - this.speed;
+        }
+        this.x = Math.floor(this.x - this.speed);
+        this.x2 = Math.floor(this.x2 - this.speed);
+    }
+    draw(){
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.x1, this.y, this.width, this.height);
+    }
+}
+
+const layer1 = new Layer(backgroundLayer1, 0.2);
+const layer2 = new Layer(backgroundLayer2, 0.4);
+const layer3 = new Layer(backgroundLayer3, 0.6);
+const layer4 = new Layer(backgroundLayer4, 0.8);
+const layer5 = new Layer(backgroundLayer5, 1);
+
+const gameObjects = [layer1, layer2, layer3, layer4, layer5];
+
 function animate(){
     ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
-    //ctx.drawImage(playerImage, frameX, frameY, spriteWidth, spriteHeight, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.drawImage(backgroundLayer4, x, 0, spriteWidth, spriteHeight, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.drawImage(backgroundLayer4, x2, 0, spriteWidth, spriteHeight, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    if (x < -2400) x = 2400;
-    else x -= gameSpeed;
-    if (x2 < -2400) x2 = 2400;
-    else x2 -= gameSpeed;
+    gameObjects.forEach(object => {
+        object.update();
+        object.draw();
+    });
     requestAnimationFrame(animate);
 }
 animate();
