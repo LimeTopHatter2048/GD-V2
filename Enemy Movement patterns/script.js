5d312e3c63b36aca41684090711f492eef887bd1
Leonaitasi Mafileo <leonaitasim@gmail.com>
2025-05-08 15:51:07 +1200

    forEach(enemy =>{});

--- Enemy Movement patterns/script.js
+++ Enemy Movement patterns/script.js
@@ -3,6 +3,8 @@ const canvas =document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width =500/2;
 CANVAS_HEIGHT = canvas.height =1000/2;
+const numberOfEnemies = 100;
+const enemiesArray = [];
 
 class Enemy {
     constructor(){
@@ -10,24 +12,28 @@ class Enemy {
         this.y = Math.random()*canvas.height;
         this.width = 20;
         this.height = 20;
+        this.speed = Math.random() * 4 - 2;
     }
     update(){
-        this.x++;
-        this.y++;
+        this.x += this.speed;
+        this.y += this.speed;
     }
     draw(){
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.strokeRect(this.x, this.y, this.width, this.height);
     }
 };
-const enemy1 = new Enemy();
-const enemy2 = new Enemy();
 
+for (let i = 0; i < numberOfEnemies; i++){
+    enemiesArray.push(new Enemy());
+}
+
+console.log(enemiesArray);
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    enemy1.update();
-    enemy2.update();
-    enemy1.draw();
-    enemy2.draw();
+    enemiesArray.forEach(enemy => {
+        enemy.update();
+        enemy.draw();
+    });
     requestAnimationFrame(animate);
 }
 animate();
