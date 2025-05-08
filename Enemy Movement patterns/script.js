abe04315cef06f63b4be49d481a21c150c118248
Leonaitasi Mafileo <leonaitasim@gmail.com>
2025-05-08 16:36:59 +1200

    enemy wiggling movement- update() randomize position - & enemy appears in the canvas with it full width.

--- Enemy Movement patterns/script.js
+++ Enemy Movement patterns/script.js
@@ -3,7 +3,7 @@ const canvas =document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width =500/2;
 CANVAS_HEIGHT = canvas.height =1000/2;
-const numberOfEnemies = 100;
+const numberOfEnemies = 10;
 const enemiesArray = [];
 
 let gameFrame = 0;
@@ -12,19 +12,19 @@ class Enemy {
     constructor(){
         this.image = new Image();
         this.image.src = 'img/enemy1.png';
-        this.x = Math.random()*canvas.width;
-        this.y = Math.random()*canvas.height;
-        this.speed = Math.random() * 4 - 2;
+        //this.speed = Math.random() * 4 - 2;
         this.spriteWidth = 293;
         this.spriteHeight = 155;
-        this.width = this.spriteWidth/3;
-        this.height = this.spriteHeight/3;
+        this.width = this.spriteWidth/4;
+        this.height = this.spriteHeight/4;
+        this.x = Math.random()*(canvas.width - this.width);
+        this.y = Math.random()*(canvas.height - this.height);
         this.frame = 0;
         this.frameSpeed = Math.floor(Math.random() * 3 + 1); //To get rid of decimal points interfere with the updating image 
     }
     update(){
-        this.x += this.speed;
-        this.y += this.speed;
+        this.x += Math.random() * 5 - 2.5;
+        this.y += Math.random() * 5 - 2.5;
         // animate sprites
         if (gameFrame % this.frameSpeed === 0){
             this.frame > 4 ? this.frame = 0 : this.frame++;
