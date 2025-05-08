059800e61dfd802ef1534084bb7ffa9453a41b89
Leonaitasi Mafileo <leonaitasim@gmail.com>
2025-05-08 15:40:22 +1200

    class Enemy {};

--- Enemy Movement patterns/script.js
+++ Enemy Movement patterns/script.js
@@ -4,18 +4,30 @@ const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width =500/2;
 CANVAS_HEIGHT = canvas.height =1000/2;
 
-enemy1 = {
-    x: 10,
-    y: 50,
-    width: 20,
-    height: 20,
-}
+class Enemy {
+    constructor(){
+        this.x = Math.random()*canvas.width;
+        this.y = Math.random()*canvas.height;
+        this.width = 20;
+        this.height = 20;
+    }
+    update(){
+        this.x++;
+        this.y++;
+    }
+    draw(){
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+};
+const enemy1 = new Enemy();
+const enemy2 = new Enemy();
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    enemy1.x++;
-    enemy1.y++;
-    ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
+    enemy1.update();
+    enemy2.update();
+    enemy1.draw();
+    enemy2.draw();
     requestAnimationFrame(animate);
 }
 animate();
