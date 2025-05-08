6afcca19675814bc3a02fb6c3b578f31a12d43dc
Leonaitasi Mafileo <leonaitasim@gmail.com>
2025-05-09 10:36:19 +1200

    canvas.getBoundingClientRect() -off set point "e.x - canvasPosition.left" -set to mid "-this.width&height/2"

--- collision_detection/script.js
+++ collision_detection/script.js
@@ -1,32 +1,47 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = 500;
-canvas.height = 700;
+canvas.height = 500;
 const explosions = [];
+let canvasPosition = canvas.getBoundingClientRect();
 
 class Explosion {
     constructor(x, y){
-        this.x = x;
-        this.y = y;
         this.spriteWidth = 1000/5;
         this.spriteHeight = 179;
         this.width = this.spriteWidth/2;
         this.height = this.spriteHeight/2;
+        this.x = x-this.width/2;
+        this.y = y-this.height/2;
         this.image = new Image();
-        this.image.src = 'boom.png';
+        this.image.src = 'img/boom.png';
         this.frame = 0;
+        this.timer = 0;
     }
     update(){
-        this.frame++;
+        this.timer++;
+        if (this.timer % 10 === 0){
+            this.frame++;
+        }
     }
     draw(){
         //ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
-        ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height)
+        ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 }
 
 window.addEventListener('click', function(e){
-    console.log(e);
-    ctx.fillStyle = 'white';
-    ctx.fillRect(e.x, e.y, 50, 50);
+    let positionX = e.x - canvasPosition.left;
+    let positionY = e.y - canvasPosition.top;
+    explosions.push(new Explosion(positionX, positionY));
 });
+
+function animate(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < explosions.length; i++){
+        explosions[i].update();
+        explosions[i].draw();
+    }
+    requestAnimationFrame(animate);
+};
+animate();
