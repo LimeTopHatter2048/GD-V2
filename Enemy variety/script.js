71dbf7695e9d0eea8f47c2b41a440093adc965cf
Leonaitasi Mafileo <leonaitasim@gmail.com>
2025-05-12 13:16:42 +1200

    class child "super()"-child class will pull unknown variables from parent class. -in derived (child) classes SUPER must be called before you can use "THIS." keyword

--- Enemy variety/script.js
+++ Enemy variety/script.js
@@ -14,6 +14,7 @@ window.addEventListener('load',function(){
             this.enemyTimer = 0;
         }
         update(deltaTime){
+            this.enemies = this.enemies.filter(object => !object.markedForDeletion);
             if (this.enemyTimer > this.enemyInterval){
                 this.#addNewEnemy();
                 this.enemyTimer = 0;
@@ -28,7 +29,7 @@ window.addEventListener('load',function(){
             this.enemies.forEach(Object => Object.draw(this.ctx));
         }
         #addNewEnemy(){
-            this.enemies.push(new Enemy(this));
+            this.enemies.push(new Worm(this));
         }
     }
 
@@ -36,16 +37,29 @@ window.addEventListener('load',function(){
         constructor(game){
             this.game = game;
             console.log(this.game);
-            this.x= this.game.width;
-            this.y=Math.random() * this.game.height;
-            this.width=100;
-            this.height=100;
+            this.markedForDeletion = false;
         }
         update(){
             this.x--;
+            // remove enemies
+            if (this.x < 0 - this.width) this.markedForDeletion = true;
         }
         draw(ctx){
-            ctx.fillRect(this.x, this.y, this.width, this.height);
+            //ctx.fillRect(this.x, this.y, this.width, this.height);
+            ctx.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+        }
+    }
+
+    class Worm extends Enemy {
+        constructor(game){
+            super(game);
+            this.spriteWidth = 229;
+            this.spriteHeight = 171;
+            this.width=this.spriteWidth/2;
+            this.height=this.spriteHeight/2;
+            this.x= this.game.width;
+            this.y=Math.random() * this.game.height;
+            this.image = worm;
         }
     }
 
