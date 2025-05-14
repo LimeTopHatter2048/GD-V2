cdd1199329346327da16eecffd3dfb3b5faccb23
Leonaitasi Mafileo <leonaitasim@gmail.com>
2025-05-14 19:29:26 +1200

    reposition hitbox context.arc(x, y, radius, startAngle, endAngle, anticlockwise); x	X coordinate of the center of the circle y	Y coordinate of the center of the circle radius	Radius of the circle startAngle	Angle (in radians) to start the arc — 0 is to the right endAngle	Angle (in radians) to end the arc anticlockwise (optional)	false = clockwise (default), true = counterclockwise

--- project7-scroller/script.js
+++ project7-scroller/script.js
@@ -77,20 +77,20 @@ window.addEventListener('load', function(){
             this.frameY = 0;
         }
         draw(context){
-            context.strokeStyle = "white";
-            context.strokeRect(this.x, this.y, this.width, this.height);
+            context.lineWidth = 5
+            context.strokeStyle = 'white';
             context.beginPath();
-            context.arc(this.x + this.width/2, this.y + this.height/2, this.width/2, 0, Math.PI * 2);
+            context.arc(this.x + this.width/2, this.y + this.height/2 + 20, this.width/3, 0, Math.PI * 2);
             context.stroke();
             context.drawImage(this.image, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
         }
         update(input, deltaTime, enemies){
             // collision detection
             enemies.forEach(enemy => {
-                const dx = (enemy.x + enemy.width/2) - (this.x + this.width/2);
-                const dy = (enemy.y + enemy.height/2) - (this.y + this.height/2);
+                const dx = (enemy.x + enemy.width/2 - 20) - (this.x + this.width/2);
+                const dy = (enemy.y + enemy.height/2) - (this.y + this.height/2 + 20);
                 const distance = Math.sqrt(dx * dx + dy * dy);
-                if (distance < enemy.width/2 + this.width/2){
+                if (distance < enemy.width/3 + this.width/3){
                     gameOver = true;
                 }
             });
@@ -183,10 +183,10 @@ window.addEventListener('load', function(){
             this.markedForDeletion = false;
         }
         draw(context){
-            context.strokeStyle = "white";
-            context.strokeRect(this.x, this.y, this.width, this.height);
+            context.lineWidth = 5
+            context.strokeStyle = 'white';
             context.beginPath();
-            context.arc(this.x + this.width/2, this.y + this.height/2, this.width/2, 0, Math.PI * 2);
+            context.arc(this.x + this.width/2 - 20, this.y + this.height/2, this.width/3, 0, Math.PI * 2);
             context.stroke();
             context.drawImage(this.image, this.frameX * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
         }
