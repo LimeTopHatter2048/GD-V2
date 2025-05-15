5e11e5cd721010328e222e14667e974b8b53a85b
Leonaitasi Mafileo <leonaitasim@gmail.com>
2025-05-15 15:15:11 +1200

    -test3 drawImage()

--- project 9-/player.js
+++ project 9-/player.js
@@ -4,31 +4,10 @@ export class Player {
         this.image = document.getElementById("player");
         this.spriteWidth = 100;
         this.spriteHeight = 91.3;
-        this.width = this.spriteWidth/2;
-        this.height = this.spriteHeight/2;
+        this.width = this.spriteWidth/1.2;
+        this.height = this.spriteHeight/1.2;
         this.x = this.game.width/2 - this.width/2;
         this.y = this.game.height - this.height;
-    }
-    update(){
-        this.x++;
-    }
-    draw(context){
-        context.fillStyle = 'red';
-        context.fillRect(this.x, this.y, this.width, this.height);
-    }
-}
-
-/*         this.gameWidth = gameWidth;
-        this.gameHeight = gameHeight;
-        this.states = [new StandingLeft(this), new StandingRight(this), new SittingLeft(this), new SittingRight(this), new RunningLeft(this), new RunningRight(this), new JumpingLeft(this), new JumpingRight(this), new FallingLeft(this), new FallingRight(this)];
-        this.currentState = this.states[1];
-        this.image = document.getElementById("dogImage");
-        this.spriteWidth = 200; //200
-        this.spriteHeight = 181.83; //181.83
-        this.width = this.spriteWidth/2;
-        this.height = this.spriteHeight/2;
-        this.x = this.gameWidth/2 - this.width/2;
-        this.y = this.gameHeight - this.height;
         this.vy = 0;
         this.weight = 1;
         this.frameX = 0;
@@ -39,4 +18,18 @@ export class Player {
         this.frameInterval = 1000/this.fps;
         this.speed = 0;
         this.maxSpeed = 10;
-        this.markedForDeletion = false; */
+        this.markedForDeletion = false;
+    }
+    update(){
+        this.x++;
+    }
+    draw(context){
+        context.drawImage(this.image, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+    }
+}
+
+/*         this.gameWidth = gameWidth;
+        this.gameHeight = gameHeight;
+        this.states = [new StandingLeft(this), new StandingRight(this), new SittingLeft(this), new SittingRight(this), new RunningLeft(this), new RunningRight(this), new JumpingLeft(this), new JumpingRight(this), new FallingLeft(this), new FallingRight(this)];
+        this.currentState = this.states[1];
+        this.image = document.getElementById("dogImage");*/
