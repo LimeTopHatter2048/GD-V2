2ed639196ffd30960cd9de939e1ea1154c6daa17
Leonaitasi Mafileo <leonaitasim@gmail.com>
2025-05-16 15:49:17 +1200

    class Splash

--- project 9-/particles.js
+++ project 9-/particles.js
@@ -29,7 +29,24 @@ export class Dust extends Particle {
     }
 }
 export class Splash extends Particle {
-
+    constructor(game, x, y){
+        super(game);
+        this.image = document.getElementById('fire');
+        this.size = Math.random() * 100 + 100;
+        this.x = x;
+        this.y = y;
+        this.speedX = Math.random() * 6 - 3;
+        this.speedY = Math.random() * 6 - 3;
+        this.gravity = 0;
+    }
+    update(){
+        super.update();
+        this.gravity += 0.1;
+        this.y += this.gravity;
+    }
+    draw(context){
+        context.drawImage(this.image, this.x, this.y, this.size, this.size);
+    }
 }
 export class Fire extends Particle {
     constructor(game, x, y){
